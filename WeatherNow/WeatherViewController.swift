52874058662b13d1ea580e1b74623e317b5f44ae
Hurlarious <hurlarious@gmail.com>
2016-11-26 19:41:55 -0800

    storyboard, outlets, tableview delegate

---
+++ WeatherNow/WeatherViewController.swift
@@ -0,0 +1,51 @@
+//
+//  WeatherViewController.swift
+//  WeatherNow
+//
+//  Created by Dave Hurley on 2016-11-26.
+//  Copyright © 2016 Dave Hurley. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: - Variables/Properties/Outlets
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Lifecyle
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    // MARK: - TableView Delegate Methods
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+
+
+}
+
