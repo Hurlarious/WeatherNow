6a844c352dcd3372daa187a5777c57531b5d9f45
Hurlarious <hurlarious@gmail.com>
2016-11-26 16:45:49 -0800

    Initial Commit

---
+++ WeatherNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherNow
+//
+//  Created by Dave Hurley on 2016-11-26.
+//  Copyright © 2016 Dave Hurley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
